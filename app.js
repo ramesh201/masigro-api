d3746d1b6b87f5301c9f226d83df6ac2229347da
ramesh201 <rameshsheran2@gmail.com>
2020-08-25 21:42:57 +0530

    Startup Commit

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-alert("Hello masigro!!!")
+//alert("Hello masigro!!!")
+
+const express = require('express');
+/*const mongoose = require('mongoose');
+const cors = require('cors');
+const heroes = require('./routes/heroes');*/
+const home = require('./routes/home');
+/*const authenticator = require('./middlewares/authenticator');
+const mailer = require('./middlewares/emailjob');*/
+const app = express();
+const PORT = 5000;
+
+//app.use(cors());
+app.use(express.json());
+/*app.use(authenticator);
+app.use(mailer);
+app.use('/api/heroes', heroes);*/
+app.use('/', home);
+
+/*mongoose
+    .connect("mongodb://localhost/herodb", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to Db successfully ... "))
+    .catch(err => console.log("Ërror has occured while connecting to db : ", err));
+*/
+app.listen(PORT, function () {
+    console.log("Masigro - Listening on Port - " + PORT);
+});
--- home.js
+++
@@ -1,1 +0,0 @@
-alert("Home.js")
