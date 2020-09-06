5b903c0b77176e8b326e161e7ab3c48e60e2b18d
ramesh201 <rameshsheran2@gmail.com>
2020-09-06 07:31:11 +0530

    setup api

--- routes/users.js
+++ routes/users.js
@@ -21,14 +21,26 @@ router.post("/", async (req, res) => {
 
     console.log("userToken");
     */
-
+   console.log("API-0");
   try {
+    console.log("before salt");
     const saltRounds = await bcrypt.genSalt(10);
+    console.log("after salt");
     let encryptedHashPassword = await bcrypt.hash(req.body.userPassword,saltRounds);
-
+    console.log("API");
     let isExisting = await User.findOne({ userEmail: req.body.userEmail });
 console.log("exist: "+isExisting);
 if(isExisting == null){
+    console.log("im here");
+    /*let user = new User({
+        userFirstName: "FN",//req.body.userFirstName,
+        userLastName: "LN",//req.body.userLastName,
+      username: "fl",//req.body.username,
+      userEmail: "flhghhgh@fl.lk",//req.body.userEmail,
+      userPassword: encryptedHashPassword,
+      userRoles: "",//req.body.userRoles == "" ? "subscriber" : req.body.userRoles
+    });*/
+
     let user = new User({
         userFirstName: req.body.userFirstName,
         userLastName: req.body.userLastName,
@@ -38,8 +50,15 @@ if(isExisting == null){
       userRoles: req.body.userRoles == "" ? "subscriber" : req.body.userRoles
     });
 
-    user = await user.save();
-
+    console.log("im out");
+    try {
+        user = await user.save();
+        console.log("im saved");
+    } catch (error) {
+        console.log("error: "+error.message);
+    }
+    
+    console.log("after saved");
     return res.send({status:res.statusCode,users:{
         userFirstName: req.body.userFirstName,
         userLastName: req.body.userLastName,
@@ -61,5 +80,7 @@ else{
     return res.status(500).send(error.message);
   }
 });
+
+
 
 module.exports = router;
